431103b9359541e92fdd156b9969b98884204f56
Lena <2863040@ukr.net>
2019-10-10 13:04:04 +0300

    Updates

---
+++ precache-manifest.29e63392ae421bf5f74a2c9f730bc585.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5edf6b25c74806d39e54a69130829d7",
+    "url": "/goit-react-hw-04-pet-store/index.html"
+  },
+  {
+    "revision": "5ebd64e93f34ca6bc735",
+    "url": "/goit-react-hw-04-pet-store/static/css/main.c3ebac5a.chunk.css"
+  },
+  {
+    "revision": "4d5b8bd2d541351c7170",
+    "url": "/goit-react-hw-04-pet-store/static/js/2.41b03dc0.chunk.js"
+  },
+  {
+    "revision": "5ebd64e93f34ca6bc735",
+    "url": "/goit-react-hw-04-pet-store/static/js/main.194fe5d2.chunk.js"
+  },
+  {
+    "revision": "61b4497bce358e413217",
+    "url": "/goit-react-hw-04-pet-store/static/js/runtime-main.537c8fd8.js"
+  }
+]);
